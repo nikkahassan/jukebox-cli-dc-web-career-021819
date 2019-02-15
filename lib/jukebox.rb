dea116599afe2fc8b6130dfb78cd1974e4ada4c8
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-15 18:49:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,17 +25,7 @@ def list(songs)
   }
 end
 
-def play(songs)
-  puts "Please enter a song name or number:"
-    response = gets.downcase.chomp 
-      if (1..9).to_a.include?(response.to_i)
-        puts "Playing #{songs[response.to_i - 1]}"
-      elsif songs.include?(response)
-        puts "Playing #{response}"
-      else 
-        puts "Invalid input, please try again"
-  end 
-end 
+
 
 def exit_jukebox()
   puts "Goodbye"
