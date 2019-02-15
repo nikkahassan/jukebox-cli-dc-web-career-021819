bcfa00f8dbd1049bd31ad7ae8692f6daa4c92fa9
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-15 18:39:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,8 +11,8 @@ songs = [
 ]
 
 def help 
-  puts 
-"I accept the following commands:
+  puts "I accept the following commands:"
+  puts ""
 - help : displays this help message
 - list : displays a list of songs you can play
 - play : lets you choose a song to play
